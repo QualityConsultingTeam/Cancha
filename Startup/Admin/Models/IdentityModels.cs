e953798905ec226e020ef8a9bcb17d38d7d46e6b
account user <accountuser@ca2s.com>
2015-08-24 15:29:14 -0600

    identity model

--- Startup/Admin/Models/IdentityModels.cs
+++ Startup/Admin/Models/IdentityModels.cs
@@ -92,18 +92,18 @@ namespace Admin.Models
         protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            // to avoid the "has no keys" errors when running Update-Database on PM
-            modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id).ToTable("AspNetRoles");
+            //// to avoid the "has no keys" errors when running Update-Database on PM
+            //modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id).ToTable("AspNetRoles");
 
-            EntityTypeConfiguration<IdentityUser> table =  modelBuilder.Entity<IdentityUser>().ToTable("AspNetUsers");
+            //EntityTypeConfiguration<IdentityUser> table =  modelBuilder.Entity<IdentityUser>().ToTable("AspNetUsers");
 
-            table.HasMany(c => c.Claims).WithOptional().HasForeignKey(c => c.UserId);
-            table.HasMany(c => c.Logins).WithOptional().HasForeignKey(c => c.UserId);
-            table.HasMany(c => c.Roles).WithOptional().HasForeignKey(c => c.UserId);
+            //table.HasMany(c => c.Claims).WithOptional().HasForeignKey(c => c.UserId);
+            //table.HasMany(c => c.Logins).WithOptional().HasForeignKey(c => c.UserId);
+            //table.HasMany(c => c.Roles).WithOptional().HasForeignKey(c => c.UserId);
 
-            modelBuilder.Entity<IdentityUserLogin>().HasKey(l => new { l.UserId, l.LoginProvider, l.ProviderKey }).ToTable("AspNetUserLogins");
-            modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId }).ToTable("AspNetUserRoles");
-            modelBuilder.Entity<IdentityUserClaim>().HasKey(k=>k.Id).ToTable("AspNetUserClaims");
+            //modelBuilder.Entity<IdentityUserLogin>().HasKey(l => new { l.UserId, l.LoginProvider, l.ProviderKey }).ToTable("AspNetUserLogins");
+            //modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId }).ToTable("AspNetUserRoles");
+            //modelBuilder.Entity<IdentityUserClaim>().HasKey(k=>k.Id).ToTable("AspNetUserClaims");
 
 
            
